f0cd5d8072a50e0d0a3cdd6e9afb931b919232e2
Kevin Raí Salazar <71470078+krsalazar@users.noreply.github.com>
2020-09-25 20:54:35 -0600

    Update settings.py

--- ejemploAPI/api_pelis/api_pelis/settings.py
+++ ejemploAPI/api_pelis/api_pelis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nw^z&(hkvvtu*ap(p$p3&869&odp)jbm1^a@=x1hs%wd8-p*em'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
